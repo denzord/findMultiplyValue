4d5b5994bef3a28c07b512d5fa9579c45c2a7e70
denzord <45736463+denzord@users.noreply.github.com>
2020-07-05 06:23:32 +0700

    Create findValue.js
    function to replace value multiply of 3 with "fizz", and value multiply of 5 with "buzz", and both of them with "fizzbuzz"

---
+++ findValue.js
@@ -0,0 +1,7 @@
+function fizzBuzz(n) {
+    // Finding value multiply of 3 and 5 will replace with fizz and buzz
+    for (let i = 0; i < n;) {
+        let result = (++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i;
+        console.log(result);
+    };
+}
