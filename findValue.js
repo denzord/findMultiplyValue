d50e0fe8f3803ea5328e06076d1ccb5bea3f2527
denzord <45736463+denzord@users.noreply.github.com>
2020-07-05 21:55:03 +0700

    Update findValue.js
    fix result not define

--- findValue.js
+++ findValue.js
@@ -1,7 +1,8 @@
 function fizzBuzz(n) {
     // Finding value multiply of 3 and 5 will replace with fizz and buzz
+    let result = 0;
     for (let i = 0; i < n;) {
-        let result = (++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i;
+        result = (++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i;
     }
     return result;
 }
